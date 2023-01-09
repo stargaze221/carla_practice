501e6c893579bf3b0d748c92370ba9c8485c9ea0
HyungJin Yoon <stargaze221@gmail.com>
2023-01-09 15:01:54 -0800

    milestone

--- node_ego_vehicle.py
+++ node_ego_vehicle.py
@@ -30,10 +30,9 @@ except ImportError:
 from tools.settings import LOOP_FREQ
 from tools.sensors import SensorManager, DisplayManager
 from tools.map import MapImage, Util
-from tools.constants import COLOR_BLACK, COLOR_WHITE, PIXELS_PER_METER, PIXELS_AHEAD_VEHICLE, COLOR_ALUMINIUM_4
 
-#<--------- Here, let me work on it as a class
-
+from tools.constants import COLOR_BLACK, COLOR_WHITE, PIXELS_PER_METER, PIXELS_AHEAD_VEHICLE
+from tools.constants import COLOR_ALUMINIUM_4, COLOR_PLUM_0, COLOR_SKY_BLUE_0, COLOR_CHOCOLATE_1, COLOR_CHAMELEON_0, COLOR_ALUMINIUM_1
 
 
 
@@ -55,15 +54,40 @@ class MapManager(object):
         self.surface_size = self.map_image.big_map_surface.get_width()
         self.scaled_size = int(self.surface_size)
 
-        scaled_original_size = self.original_surface_size * (1.0 / 0.9)
+        scaled_original_size = self.original_surface_size * (1.5)
         self.hero_surface = pygame.Surface((scaled_original_size, scaled_original_size)).convert()
         self.result_surface = pygame.Surface((self.surface_size, self.surface_size)).convert()
         self.result_surface.set_colorkey(COLOR_BLACK)
 
+        # Render Actors
+        self.actors_surface = pygame.Surface((self.map_image.surface.get_width(), self.map_image.surface.get_height()))
+        self.actors_surface.set_colorkey(COLOR_BLACK)
+
+        self.border_round_surface = pygame.Surface((self.original_surface_size,self.original_surface_size), pygame.SRCALPHA).convert()
+        self.border_round_surface.set_colorkey(COLOR_WHITE)
+        self.border_round_surface.fill(COLOR_BLACK)
+
+        # Used for Hero Mode, draws the map contained in a circle with white border
+        center_offset = (int(self.original_surface_size / 2), int(self.original_surface_size/ 2))
+
+
+
+        # Apply clipping rect
+        clipping_rect = pygame.Rect(0,
+                                    0,
+                                    300,
+                                    300)
+        pygame.draw.rect(self.border_round_surface, COLOR_ALUMINIUM_1, clipping_rect)
+        pygame.draw.rect(self.border_round_surface, COLOR_WHITE, clipping_rect)
+        
+        
+        
+        # pygame.draw.circle(self.border_round_surface, COLOR_ALUMINIUM_1, center_offset, int(self.original_surface_size / 2))
+        # pygame.draw.circle(self.border_round_surface, COLOR_WHITE, center_offset, int((self.original_surface_size - 8) / 2))
+
+
     def observe(self):
 
-        # transform = self.ego_vehicle.get_transform()
-        # print(transform)
 
         actors = self.world.get_actors()
 
@@ -75,6 +99,86 @@ class MapManager(object):
 
         return 0
 
+    def _split_actors(self):
+        """Splits the retrieved actors by type id"""
+        vehicles = []
+        traffic_lights = []
+        speed_limits = []
+        walkers = []
+
+        for actor_with_transform in self.actors_with_transforms:
+            actor = actor_with_transform[0]
+            if 'vehicle' in actor.type_id:
+                vehicles.append(actor_with_transform)
+            elif 'traffic_light' in actor.type_id:
+                traffic_lights.append(actor_with_transform)
+            elif 'speed_limit' in actor.type_id:
+                speed_limits.append(actor_with_transform)
+            elif 'walker.pedestrian' in actor.type_id:
+                walkers.append(actor_with_transform)
+
+        return (vehicles, traffic_lights, speed_limits, walkers)
+
+    def _render_walkers(self, surface, list_w, world_to_pixel):
+        """Renders the walkers' bounding boxes"""
+        for w in list_w:
+            color = COLOR_PLUM_0
+
+            # Compute bounding box points
+            bb = w[0].bounding_box.extent
+            corners = [
+                carla.Location(x=-bb.x, y=-bb.y),
+                carla.Location(x=bb.x, y=-bb.y),
+                carla.Location(x=bb.x, y=bb.y),
+                carla.Location(x=-bb.x, y=bb.y)]
+
+            w[1].transform(corners)
+            corners = [world_to_pixel(p) for p in corners]
+            pygame.draw.polygon(surface, color, corners)
+
+    def _render_vehicles(self, surface, list_v, world_to_pixel):
+        """Renders the vehicles' bounding boxes"""
+        for v in list_v:
+            color = COLOR_SKY_BLUE_0
+            if int(v[0].attributes['number_of_wheels']) == 2:
+                color = COLOR_CHOCOLATE_1
+            if v[0].attributes['role_name'] == 'hero':
+                color = COLOR_CHAMELEON_0
+            # Compute bounding box points
+            bb = v[0].bounding_box.extent
+            # corners = [carla.Location(x=-bb.x, y=-bb.y),
+            #            carla.Location(x=bb.x - 0.8, y=-bb.y),
+            #            carla.Location(x=bb.x, y=0),
+            #            carla.Location(x=bb.x - 0.8, y=bb.y),
+            #            carla.Location(x=-bb.x, y=bb.y),
+            #            carla.Location(x=-bb.x, y=-bb.y)
+            #            ]
+            # v[1].transform(corners)
+            # corners = [world_to_pixel(p) for p in corners]
+            # pygame.draw.lines(surface, color, False, corners, int(math.ceil(4.0 * self.map_image.scale)))
+
+            corners = [
+                carla.Location(x=-bb.x, y=-bb.y),
+                carla.Location(x=bb.x, y=-bb.y),
+                carla.Location(x=bb.x, y=bb.y),
+                carla.Location(x=-bb.x, y=bb.y)]
+            v[1].transform(corners)
+            corners = [world_to_pixel(p) for p in corners]
+            pygame.draw.polygon(surface, color, corners)
+
+
+            
+    def render_actors(self, surface, vehicles, traffic_lights, speed_limits, walkers):
+        """Renders all the actors"""
+        # Static actors
+        # self._render_traffic_lights(surface, [tl[0] for tl in traffic_lights], self.map_image.world_to_pixel)
+        # self._render_speed_limits(surface, [sl[0] for sl in speed_limits], self.map_image.world_to_pixel,
+        #                           self.map_image.world_to_pixel_width)
+
+        # Dynamic actors
+        self._render_vehicles(surface, vehicles, self.map_image.world_to_pixel)
+        self._render_walkers(surface, walkers, self.map_image.world_to_pixel)
+
 
     def render(self):
 
@@ -82,14 +186,30 @@ class MapManager(object):
 
         self.result_surface.fill(COLOR_BLACK)
 
+        # Split the actors by vehicle type id
+        vehicles, traffic_lights, speed_limits, walkers = self._split_actors()
+
+
         # Zoom in and out
-        scale_factor = 0.5
+        scale_factor = 0.3
         self.scaled_size = int(self.map_image.width * scale_factor)
         self.map_image.scale_map(scale_factor)
 
+        # Render Actors
+        self.actors_surface.fill(COLOR_BLACK)
+        self.render_actors(
+            self.actors_surface,
+            vehicles,
+            traffic_lights,
+            speed_limits,
+            walkers)
+
+
+
+
         # Blit surfaces
         surfaces = ((self.map_image.surface, (0, 0)),
-                    (self.map_image.surface, (0, 0))
+                    (self.actors_surface, (0, 0))
                     )
 
         angle = 0.0 if self.hero_actor is None else self.hero_transform.rotation.yaw + 90.0
@@ -112,22 +232,38 @@ class MapManager(object):
 
             Util.blits(self.result_surface, surfaces)
 
+            self.border_round_surface.set_clip(clipping_rect)
+
 
             self.hero_surface.fill(COLOR_ALUMINIUM_4)
             self.hero_surface.blit(self.result_surface, (-translation_offset[0],
                                                          -translation_offset[1]))
 
-            rotated_result_surface = pygame.transform.rotozoom(self.hero_surface, angle, 0.9).convert()
+            rotated_result_surface = pygame.transform.rotozoom(self.hero_surface, angle, 1).convert()
 
             display_size = self.display_man.get_display_size()
 
             center = (display_size[0] / 2, display_size[1] / 2)
             rotation_pivot = rotated_result_surface.get_rect(center=center)
             offset = self.display_man.get_display_offset(self.display_pos)
-            
+
+
+            ### <--- Temp for clipping 
+
+
+            # clipping_rect = pygame.Rect(0,
+            #                             0,
+            #                             self.hero_surface.get_width(),
+            #                             self.hero_surface.get_height())
+
+            # rotated_result_surface.set_clip(clipping_rect)
+
+            ### <--- Temp for clipping
 
 
             self.display_man.display.blit(rotated_result_surface, (offset[0]+rotation_pivot[0], offset[1]+rotation_pivot[1]))
+
+            self.display_man.display.blit(self.border_round_surface, (offset[0], offset[1]))
 
 
 
@@ -178,6 +314,7 @@ def run_carla_node(args, client):
 
         # Instanciating the vehicle to which we attached the sensors
         bp = world.get_blueprint_library().filter('charger_2020')[0]
+        bp.set_attribute('role_name', 'hero')
         vehicle = world.spawn_actor(bp, random.choice(world.get_map().get_spawn_points()))
 
         print(vehicle.id)
@@ -199,6 +336,10 @@ def run_carla_node(args, client):
         # If can easily configure the grid and the total window size
         display_manager = DisplayManager(grid_size=[3, 4], window_size=[args.width, args.height])
 
+
+        # Map Manager
+        MapManager(world, display_manager, vehicle, display_pos=[0,3])
+
         # Then, SensorManager can be used to spawn RGBCamera, LiDARs and SemanticLiDARs as needed
         # and assign each of them to a grid position, 
         SensorManager(world, display_manager, 'RGBCamera', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=-90)), 
@@ -209,12 +350,8 @@ def run_carla_node(args, client):
                       vehicle, {}, display_pos=[0, 2])
         SensorManager(world, display_manager, 'RGBCamera', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=180)), 
                       vehicle, {}, display_pos=[1, 1])
-
-        SensorManager(world, display_manager, 'LiDAR', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=+00)), 
-                      vehicle, {'channels' : '64', 'range' : '100',  'points_per_second': '250000', 'rotation_frequency': '20'}, display_pos=[0, 3])
-        SensorManager(world, display_manager, 'SemanticLiDAR', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=+00)), 
-                      vehicle, {'channels' : '64', 'range' : '100', 'points_per_second': '100000', 'rotation_frequency': '20'}, display_pos=[1, 3])
-
+        SensorManager(world, display_manager, 'LiDAR', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=+90)), 
+                      vehicle, {'channels' : '64', 'range' : '100',  'points_per_second': '250000', 'rotation_frequency': '20'}, display_pos=[1, 3])
         SensorManager(world, display_manager, 'SemanticSegmenation', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=-90)), 
                       vehicle, {}, display_pos=[2, 0])
         SensorManager(world, display_manager, 'SemanticSegmenation', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=+00)), 
@@ -223,13 +360,6 @@ def run_carla_node(args, client):
                       vehicle, {}, display_pos=[2, 2])
         SensorManager(world, display_manager, 'SemanticSegmenation', carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=180)), 
                       vehicle, {}, display_pos=[2, 3])
-
-        # Map Manager
-        mapmanager = MapManager(world, display_manager, vehicle, display_pos=[1,0])
-
-
-
-        
 
         #Simulation loop
         call_exit = False
@@ -275,7 +405,6 @@ def run_carla_node(args, client):
 
             if call_exit:
                 break
-
             
 
             # Sleep for Set Rate
