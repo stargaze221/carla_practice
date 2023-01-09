2748114d30bb8965b70254023fdd971d2f6e1214
HYUNG JIN YOON <stargaze221@gmail.com>
2023-01-09 11:47:48 -0800

    some progress

--- node_ego_vehicle.py
+++ node_ego_vehicle.py
@@ -23,15 +23,16 @@ try:
     import pygame
     from pygame.locals import K_ESCAPE
     from pygame.locals import K_q
+    from pygame.locals import K_p
 except ImportError:
     raise RuntimeError('cannot import pygame, make sure pygame package is installed')
 
 from tools.settings import LOOP_FREQ
 from tools.sensors import SensorManager, DisplayManager
-from tools.map import MapImage
+from tools.map import MapImage, Util
+from tools.constants import COLOR_BLACK, COLOR_WHITE, PIXELS_PER_METER, PIXELS_AHEAD_VEHICLE, COLOR_ALUMINIUM_4
 
 #<--------- Here, let me work on it as a class
-PIXELS_PER_METER = 1
 
 
 
@@ -42,19 +43,22 @@ class MapManager(object):
 
         self.world = world
         self.town_map = world.get_map()
+        self.display_pos = display_pos
         self.display_man = display_manager
         self.display_man.add_sensor(self)
         self.hero_actor = vehicle
 
-        self.mapimage = MapImage(world, self.town_map, PIXELS_PER_METER, False, False, False)
+        self.map_image = MapImage(world, self.town_map, PIXELS_PER_METER, False, False, False)
 
         # I need to plot it in the surface below. At this moment 
-        self.surface = None
+        self.original_surface_size = min(self.display_man.get_display_size())
+        self.surface_size = self.map_image.big_map_surface.get_width()
+        self.scaled_size = int(self.surface_size)
 
-        
-
-
-
+        scaled_original_size = self.original_surface_size * (1.0 / 0.9)
+        self.hero_surface = pygame.Surface((scaled_original_size, scaled_original_size)).convert()
+        self.result_surface = pygame.Surface((self.surface_size, self.surface_size)).convert()
+        self.result_surface.set_colorkey(COLOR_BLACK)
 
     def observe(self):
 
@@ -69,19 +73,70 @@ class MapManager(object):
         if self.hero_actor is not None:
             self.hero_transform = self.hero_actor.get_transform()
 
-        print(self.hero_transform)
-
         return 0
-        
 
 
     def render(self):
 
         self.observe()
 
-        if self.surface is not None:
+        self.result_surface.fill(COLOR_BLACK)
+
+        # Zoom in and out
+        scale_factor = 0.5
+        self.scaled_size = int(self.map_image.width * scale_factor)
+        self.map_image.scale_map(scale_factor)
+
+        # Blit surfaces
+        surfaces = ((self.map_image.surface, (0, 0)),
+                    (self.map_image.surface, (0, 0))
+                    )
+
+        angle = 0.0 if self.hero_actor is None else self.hero_transform.rotation.yaw + 90.0
+
+        if self.hero_actor is not None:
+            # Hero Mode
+            hero_location_screen = self.map_image.world_to_pixel(self.hero_transform.location)
+            hero_front = self.hero_transform.get_forward_vector()
+            translation_offset = (hero_location_screen[0] - self.hero_surface.get_width() / 2 + hero_front.x * PIXELS_AHEAD_VEHICLE,
+                                  (hero_location_screen[1] - self.hero_surface.get_height() / 2 + hero_front.y * PIXELS_AHEAD_VEHICLE))
+
+            # Apply clipping rect
+            clipping_rect = pygame.Rect(translation_offset[0],
+                                        translation_offset[1],
+                                        self.hero_surface.get_width(),
+                                        self.hero_surface.get_height())
+
+
+            self.result_surface.set_clip(clipping_rect)
+
+            Util.blits(self.result_surface, surfaces)
+
+
+            self.hero_surface.fill(COLOR_ALUMINIUM_4)
+            self.hero_surface.blit(self.result_surface, (-translation_offset[0],
+                                                         -translation_offset[1]))
+
+            rotated_result_surface = pygame.transform.rotozoom(self.hero_surface, angle, 0.9).convert()
+
+            display_size = self.display_man.get_display_size()
+
+            center = (display_size[0] / 2, display_size[1] / 2)
+            rotation_pivot = rotated_result_surface.get_rect(center=center)
             offset = self.display_man.get_display_offset(self.display_pos)
-            self.display_man.display.blit(self.surface, offset)
+            
+
+
+            self.display_man.display.blit(rotated_result_surface, (offset[0]+rotation_pivot[0], offset[1]+rotation_pivot[1]))
+
+
+
+
+        #self.map_image.big_map_surface.set_clip(clipping_rect)
+
+        #offset = self.display_man.get_display_offset(self.display_pos)
+        #self.display_man.display.blit(self.map_image.big_map_surface, offset)
+
 
 
 
@@ -181,6 +236,7 @@ def run_carla_node(args, client):
         clock = pygame.time.Clock()
 
         start_tick = pygame.time.get_ticks()
+        if_auto_pilot = False
 
         while True:
             # Carla Tick
@@ -212,6 +268,10 @@ def run_carla_node(args, client):
                 elif event.type == pygame.KEYDOWN:
                     if event.key == K_ESCAPE or event.key == K_q:
                         call_exit = True
+                    elif event.key == K_p:
+                        if_auto_pilot ^= True
+                        vehicle.set_autopilot(if_auto_pilot)
+
 
             if call_exit:
                 break
